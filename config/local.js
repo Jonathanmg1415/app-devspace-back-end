84f36b93af3930c8badb49515018b30104baf44e
Jonathanmg1415 <53263709+Jonathanmg1415@users.noreply.github.com>
2026-07-01 19:35:33 -0500

    refactor: eliminar configuración de seguridad CORS del archivo local.js

--- config/local.js
+++ config/local.js
@@ -1,12 +1,3 @@
 module.exports = {
   prefix: '/api',
-  security: {
-    cors: {
-      allRoutes:           true,
-      allowOrigins:        ['http://localhost:9000', 'http://localhost:5173', 'http://localhost:3000'],
-      allowCredentials:    false,
-      allowRequestMethods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-      allowRequestHeaders: 'Content-Type, Authorization',
-    },
-  },
 };
